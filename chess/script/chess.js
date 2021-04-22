fcb04483348e5cc27e1c55f426a3c899feb297ed
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2021-04-22 23:12:53 +0200

    V2021.04.22

---
+++ chess/script/chess.js
@@ -0,0 +1,7 @@
+let stockfish = new Worker(
+  "../../stockfish/node_modules/stockfish/src/stockfish.js"
+);
+stockfish.onmessage = function (event) {
+  console.log(event.data ? event.data : event);
+};
+stockfish.postMessage("go depth 15");
