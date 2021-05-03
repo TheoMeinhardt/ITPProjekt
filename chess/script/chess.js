69fda4287ce19f140bca880edfe3f2b0000d43d0
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2021-05-03 16:00:35 +0200

    V2021.05.03

--- chess/script/chess.js
+++ chess/script/chess.js
@@ -1,7 +1,43 @@
-let stockfish = new Worker(
-  "../../stockfish/node_modules/stockfish/src/stockfish.js"
-);
+let stockfish = new Worker("../../stockfish/node_modules/stockfish/src/stockfish.js");
+let uciok = false;
+let board = document.querySelector("#board");
+board.appendChild(buildBoard(board));
+function generateFen() {
+    let fen;
+    return fen;
+}
+function buildBoard(board) {
+    let chars = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    let lastSquareWasWhite = false;
+    for (let i = 0; i < 8; i++) {
+        let rank = document.createElement("div");
+        rank.id = "rank";
+        for (let j = 0; j < 8; j++) {
+            let square = document.createElement("div");
+            square.id = chars[j] + (8 - i);
+            square.innerHTML = square.id;
+            if (!lastSquareWasWhite) {
+                square.classList.add("whiteSquare");
+                lastSquareWasWhite = true;
+            }
+            else {
+                square.classList.add("blackSquare");
+                lastSquareWasWhite = false;
+            }
+            rank.appendChild(square);
+        }
+        lastSquareWasWhite = lastSquareWasWhite ? false : true;
+        board.appendChild(rank);
+    }
+    return board;
+}
 stockfish.onmessage = function (event) {
-  console.log(event.data ? event.data : event);
+    console.log(event.data ? event.data : event);
+    uciok = event.data ? true : false;
 };
-stockfish.postMessage("go depth 15");
+stockfish.postMessage("uci");
+stockfish.postMessage("ucinewgame");
+stockfish.postMessage("isready");
+stockfish.postMessage("position startpos");
+stockfish.postMessage("position ");
+stockfish.postMessage("go ponder depth 15");
